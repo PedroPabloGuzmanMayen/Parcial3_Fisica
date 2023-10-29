81753138581020c1e979e6e4dca02a9579ed5985
PedroPabloGuzmanMayen <ppguzmanmayen@gmail.com>
2023-10-29 16:53:20 -0600

    Comentarios

--- Parcial3_Fisca3.py
+++ Parcial3_Fisca3.py
@@ -9,19 +9,21 @@ from mpl_interactions import ioff, panhandler, zoom_factory
 import matplotlib.patches as patches
 import numpy as np
 import math
-
+#Esta función sirve para determinar si los valores ingresados por el usuario son válidos
 def is_valid_number(value):
     try:
         float(value)
         return True
     except ValueError:
         return False
-
+#Esta es la primera ventana del programa
 class Window1:
     def __init__(self, master):
+        #Definir los aspectos importantes de la interfaz
         self.master = master
         self.master.title("Parcial 3 Fisica 3")
         self.master.geometry("1000x1000")
+        #Variables que sirven para definir los electrodomésticos que se van a conectar
         self.lavadora = tk.StringVar(value = "0") #1
         self.refrigeradora = tk.StringVar(value = "0") #2
         self.estufa = tk.StringVar(value = "0") #3
@@ -42,6 +44,7 @@ class Window1:
         self.cocina = tk.StringVar(value = "0") #18
         self.licuadora = tk.StringVar(value = "0") #19
         self.horno = tk.StringVar(value = "0") #20
+        #Sirve para guardar los electrodomésticos que se van a conectar
         tk.Label(text = "Selecciona los electrodomésticos que vas a concectar").pack()
         tk.Checkbutton(text = "Lavadora", variable = self.lavadora, onvalue = "Lavadora", offvalue = "0").pack()
         tk.Checkbutton(text = "Refrigeradora", variable = self.refrigeradora, onvalue = "Refrigeradora", offvalue = "0").pack()
@@ -63,6 +66,7 @@ class Window1:
         tk.Checkbutton(text = "Freidora", variable = self.cocina, onvalue = "Freidora", offvalue = "0").pack()
         tk.Checkbutton(text = "Licuadora", variable = self.licuadora, onvalue = "Licuadora", offvalue = "0").pack()
         tk.Checkbutton(text = "Horno", variable = self.horno, onvalue = "Horno", offvalue = "0").pack()
+        #Aquí se guarda el largo del cable
         tk.Label(text= "Selecciona el largo del cable (en metros)").pack()
         self.lenght = tk.Entry()
         self.lenght.pack()
@@ -72,6 +76,7 @@ class Window1:
         tk.Button(text = "Siguiente", command = self.next).pack()
 
     def next(self):
+        #Se crea una lista con los posibles electrodomésticos que se pueden conectar
         self.list = []
         self.list.append(self.lavadora.get())
         self.list.append(self.refrigeradora.get())
@@ -93,26 +98,26 @@ class Window1:
         self.list.append(self.cocina.get())
         self.list.append(self.licuadora.get())
         self.electrodomesticos = list(filter(lambda x: x != "0", self.list))
-
+        #Se verifica que los datos ingresados sean válidos
         if (len(self.electrodomesticos) == 0 or len(self.electrodomesticos) > 10):
             tk.messagebox.showerror("Error", "Selecciona entre 1 y 10 electrodomésticos")
         elif (is_valid_number(self.lenght.get()) == False or self.lenght.get() == "0"):
             tk.messagebox.showerror("Error", "Ingresa un largo válido")
         else:
+            #Aquí se filtran los dispositivos seleccionados por el usuario
             self.final_lenght = self.lenght.get()
             self.potencias = []
             self.voltajes = []
             self.corrientes = []
             self.horas = []
+            #Se pasa a la siguiente ventana
             self.master.destroy()
             Window2(self.electrodomesticos, self.final_lenght, self.electrodomesticos[0], 0, self.voltajes, self.corrientes, self.potencias, self.horas)
 
-
-
-
-
+#Esta ventana sirve para la selección de datos de cada electrodoméstico
 class Window2:
     def __init__(self, list, lenght, name, index, voltajes, corrientes, potencias, horas):
+        #Definir el voltaje, las horas de uso y la corriente de cada electrodoméstico
         self.Corrientes = corrientes
         self.Voltajes = voltajes
         self.Potencias = potencias
@@ -120,6 +125,7 @@ class Window2:
         self.index = index
         self.list = list
         self.lenght = lenght
+        #Definir los aspectos importantes de la interfaz
         self.name = name
         self.master = tk.Tk()
         self.master.title("Selección de datos")
@@ -140,15 +146,10 @@ class Window2:
         tk.Button(text = "Siguiente", command= self.next).pack()
         self.master.mainloop()
 
-    def is_valid_number(self, value):
-        try:
-            float(value)
-            return True
-        except ValueError:
-            return False
+
 
     def next(self):
-        pass
+        #Se verifica que los datos ingresados sean válidos
         if (is_valid_number(self.Voltaje.get()) == False or self.Voltaje.get() == "0"):
             tk.messagebox.showerror("Error", "Ingresa un voltaje válido")
         elif (is_valid_number(self.Corriente.get())== False or self.Corriente.get() == "0"):
@@ -160,11 +161,12 @@ class Window2:
         elif(float(self.Corriente.get())*float(self.Voltaje.get()) != float(self.Potencia.get())):
             tk.messagebox.showerror("Error", "La potencia no coincide con el voltaje y la corriente")
         else:
+            #Se guardan los datos ingresados por el usuario
             self.Corrientes.append(float(self.Corriente.get()))
             self.Voltajes.append(float(self.Voltaje.get()))
             self.Potencias.append(float(self.Potencia.get()))
             self.Horas.append(float(self.horas.get()))
-
+            #Se verifica si ya se ingresaron todos los electrodomésticos
             if (self.index < (len(self.list)-1)):
                 self.index += 1
                 self.name = self.list[self.index]
@@ -173,12 +175,13 @@ class Window2:
             else:
                 self.master.destroy()
                 Window3(self.list, self.lenght, self.Voltajes, self.Corrientes, self.Potencias, self.Horas)
-
+#La última ventana sirve para mostrar los resultados
 class Window3:
     def __init__(self, list, lenght, voltajes, corrientes, potencias, horas):
-  
+        #Se guardan los datos ingresados por el usuario
         self.list = list
         self.lenght = lenght
+        #Se crea la ventana y se definen los aspectos importantes de esta
         self.master = tk.Tk()
         self.master.title("Resultados")
         self.master.geometry("1000x1000")
@@ -199,38 +202,40 @@ class Window3:
         tk.Label(text= "Su factura es de: " + self.factura(potencias, horas)).pack()
                  
         self.master.mainloop()
+    #Esta función dibuja los electrodomésticos y el cable
     def draw(self):
         num_images = len(self.list)
         image_size = 100
         total_width = num_images * (image_size + 20)
         if num_images >= 2:
-              # Adjust 20 for separation
+   
             self.ax.set_xlim(0, total_width)
 
         for i in range(num_images):
-            x = i * (image_size + 20)  # Adjust 20 for separation
+            x = i * (image_size + 20) 
             self.ax.imshow(np.asarray(Image.open(self.list[i] + ".jpg")), extent=[x, x + image_size, 0, image_size])
 
         line = patches.Polygon([[0, -5], [total_width, -5]], closed=None, fill=None, edgecolor='#B87333')
         self.ax.add_patch(line)
     
         self.canvas.draw()
-
+    #Esta función sirve para calcular la energía consumida
     def calculateEnergy(self, potencias, horas):
         totalenergy = 0
         for i in range(len(potencias)):
-            totalenergy += (potencias[i]/1000)*horas[i]*30
+            totalenergy += (potencias[i]/1000)*horas[i]*30 #Multiplicar la energía por el número de horas y días, trasnformar a kilowatts
         return totalenergy
-        
+    #Calcula el diámetro del cable
     def calculateDiameter(self, potencias, corrientes, length):
         
-        maxVal = max(potencias)
-        maxIndex = potencias.index(maxVal)
-        current = corrientes[maxIndex]
-        diameter = 2*current*math.sqrt((1.72e-8*length)/(np.pi*maxVal))
+        maxVal = max(potencias) #Encontrar el valor máximo de potencia
+        maxIndex = potencias.index(maxVal) #Encuentra el índice del valor máximo
+        current = corrientes[maxIndex] #Usa la corriente del dispositivo con mayor potencia
+        diameter = 2*current*math.sqrt((1.72e-8*length)/(np.pi*maxVal)) #Calcula el diámetro
         return diameter
-    
+    #Calcula el diámetro del cable
     def calibre(self, diameter):
+    
         if diameter < 0.163:
             return "14"
         elif 0.163 < diameter < 0.205:
@@ -245,7 +250,7 @@ class Window3:
             return "5"
         elif 0.462 < diameter < 0.519:
             return "4"
-        
+    #Calcula el tipo de factura
     def factura(self, potencias, horas):
         if self.calculateEnergy(potencias, horas) <= 11:
             return "Baja tensión"
